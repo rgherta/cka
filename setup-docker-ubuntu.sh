939e2302eae0668b17018caf4803730a86303215
roman.gherta <roman.gherta@gmail.com>
2020-12-27 19:32:56 +0100

    CKA docker contained script

--- setup-docker-ubuntu.sh
+++ setup-docker-ubuntu.sh
@@ -5,18 +5,18 @@
 
 # Update the apt package index and install packages to allow apt to use a repository over HTTPS:
 apt update
-apt-get install apt-transport-https ca-certificates curl gnupg-agent software-properties-common
+apt-get -y install apt-transport-https ca-certificates curl gnupg-agent software-properties-common
 
 
 # Add Docker’s official GPG key:
 curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
 
 # Use the following command to set up the stable repository
- sudo add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable"
+add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable"
 
 # Update the apt package index, and install the latest version of Docker Engine and containerd
 apt-get update
-apt-get install docker-ce docker-ce-cli containerd.io
+apt-get -y install docker-ce docker-ce-cli containerd.io
 
 
 [ ! -d /etc/docker ] && mkdir /etc/docker
