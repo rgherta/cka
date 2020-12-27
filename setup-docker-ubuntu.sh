fc5b78b8f75882e328a2f0a9e5dff9b9424f6e9b
roman.gherta <roman.gherta@gmail.com>
2020-12-27 19:53:32 +0100

    CKA setup-kubetools for ubuntu

--- setup-docker-ubuntu.sh
+++ setup-docker-ubuntu.sh
@@ -9,7 +9,7 @@ apt-get -y install apt-transport-https ca-certificates curl gnupg-agent software
 
 
 # Add Docker’s official GPG key:
-curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg | apt-key add -
 
 # Use the following command to set up the stable repository
 add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable"
---
+++ setup-kubetools-ubuntu.sh
@@ -0,0 +1,45 @@
+#!/bin/bash
+# kubeadm installation instructions as on
+# https://kubernetes.io/docs/setup/production-environment/tools/kubeadm/install-kubeadm/
+
+#disable firewall
+ufw disable
+
+#disable AppArmor
+/etc/init.d/apparmor stop
+/etc/init.d/apparmor teardown
+
+# disable swap  swapoff/swapon
+swapoff -a  
+
+#install kubeadm kubelet kubectl
+apt-get update 
+apt-get install -y apt-transport-https curl
+curl -s https://packages.cloud.google.com/apt/doc/apt-key.gpg | apt-key add -
+cat <<EOF | sudo tee /etc/apt/sources.list.d/kubernetes.list
+deb https://apt.kubernetes.io/ kubernetes-xenial main
+EOF
+apt-get update
+apt-get install -y kubelet kubeadm kubectl
+apt-mark hold kubelet kubeadm kubectl
+
+systemctl enable --now kubelet
+
+# As a requirement for your Linux Node's iptables to correctly see bridged traffic, 
+# you should ensure net.bridge.bridge-nf-call-iptables is set to 1 in your sysctl config, e.g.
+cat <<EOF | sudo tee /etc/sysctl.d/k8s.conf
+net.bridge.bridge-nf-call-ip6tables = 1
+net.bridge.bridge-nf-call-iptables = 1
+EOF
+sysctl --system
+
+# Configure cgroup driver used by kubelet on control-plane node
+# When using Docker, kubeadm will automatically detect the cgroup driver for the kubelet and set it in the /var/lib/kubelet/config.yaml file during runtime.
+# If you are using a different CRI, you must pass your cgroupDriver value to kubeadm init, like so:
+
+#apiVersion: kubelet.config.k8s.io/v1beta1
+#kind: KubeletConfiguration
+#cgroupDriver: <value>
+
+#systemctl daemon-reload
+#systemctl restart kubelet
